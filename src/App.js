1309b3920853399c4e64e9e9a730863c13d8785d
ndurbin <nick.durbin@gmail.com>
2019-10-22 16:15:02 -0400

    Passed down the functions with the actions, but having an issue with the remove logic I think.

--- src/App.js
+++ src/App.js
@@ -6,33 +6,25 @@ import Total from './components/Total';
 import { connect } from 'react-redux';
 import { addFeature, removeFeature } from './actions/actions';
 
-const App = (props) => {
-  const { car, additionalFeatures, additionalPrice } = props
-
-  const removeItem = item => {
-    removeFeature(item)
-  };
-
-  const buyItem = item => {
-    addFeature(item)
-  };
+const App = ({ car, additionalFeatures, additionalPrice, addFeature, removeFeature }) => {
 
   return (
     <div className="boxes">
       <div className="box">
         <Header car={car} />
-        <AddedFeatures car={car} removeItem={removeItem} />
+        <AddedFeatures car={car} removeFeature={removeFeature} />
       </div>
       <div className="box">
-        <AdditionalFeatures additionalFeatures={additionalFeatures} buyItem={buyItem} />
+        <AdditionalFeatures additionalFeatures={additionalFeatures} addFeature={addFeature} />
         <Total car={car} additionalPrice={additionalPrice} />
       </div>
     </div>
   );
 };
 
-function mapDispatchToProps() {
-
+const mapDispatchToProps = {
+  addFeature,
+  removeFeature
 }
 
 function mapStateToProps(state) {
